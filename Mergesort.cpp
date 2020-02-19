b76c2c4b052ca5e968c0f6305eebce1c0e9ded6c
Divyanshu <56086382+Divyanshu-info@users.noreply.github.com>
2020-02-19 13:47:01 +0530

    19/02/2020

---
+++ Mergesort.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+void mergesort(int a[], int left, int right){
+    if(left>right){
+        
+    }
+    int m=(l+r)/2;
+    
+}
+
+int main(int argc, char const *argv[])
+{
+    int a[10]={10,9,8,7,6,5,4,3,2,1};
+    
+
+    return 0;
+}
